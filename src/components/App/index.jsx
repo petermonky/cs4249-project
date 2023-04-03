cf157aeef8b6381f4446ff0483b7b8f8657a5acc
petermonky <wjdgustjr0554@gmail.com>
2023-04-03 09:16:23 +0800

    feat: lobby

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,12 +5,14 @@ import Scan from "../Scan"
 import PayTo from "../PayTo"
 import ShopeePay from '../ShopeePay'
 import TopUp from '../TopUp'
+import Lobby from "../Lobby"
 
 function App() {
   return (
     <div className={styles.app}>
       <Router>
         <Routes>
+          <Route path="/" element={<Lobby />} />
           <Route path="/:variant">
             <Route index element={<Main />} />
             <Route path='scan' element={<Scan />} />
---
+++ src/components/Lobby/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Lobby.module.scss"
+
+function Lobby() {
+  const variants = [...Array(6).keys()].map((x) => x + 1);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>CS4249 Project Variants</h1>
+      {variants.map((v) => (
+        <Link to={`${v}`} className={styles.link}><div><span>Variant {v}</span></div></Link>
+      ))}
+    </div>
+  );
+}
+
+export default Lobby;
